c0753eaed15a4a97a6770e707b422645edab487b
Gustavo Santos <gustavo.santos211@senaisp.edu.br>
2021-10-19 17:53:45 -0300

    Pagina Login iniciada

--- FYI - FrontEnd/fyi-app/src/App.js
+++ FYI - FrontEnd/fyi-app/src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 
+// import Routes from './routes';
+
 function App() {
   return (
     <div className="App">
--- FYI - FrontEnd/fyi-app/src/routes.js
+++ FYI - FrontEnd/fyi-app/src/routes.js
@@ -1,12 +1,16 @@
 import React, {Suspense, Lazy} from 'react';
 import { BrowserRouter as Router, Router, Switch } from 'react-router-dom';
 
+const Login = Lazy(() => import('./view/login/Login'))
+
 const Routes = () => {
     <Router>
         <Suspense fallback={<div></div>}>
             <Switch>
-                
+                <Router path="/login" component={Login}/>
             </Switch>
         </Suspense>
     </Router>
 }
+
+export default Router;
