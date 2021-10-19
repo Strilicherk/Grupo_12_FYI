ca412efae0b5879b6e831f6b382310baed99c19f
Gustavo Santos <gustavo.santos211@senaisp.edu.br>
2021-10-19 16:51:20 -0300

    React, Expo instalados e routes iniciadas

---
+++ FYI - FrontEnd/fyi-app/src/routes.js
@@ -0,0 +1,12 @@
+import React, {Suspense, Lazy} from 'react';
+import { BrowserRouter as Router, Router, Switch } from 'react-router-dom';
+
+const Routes = () => {
+    <Router>
+        <Suspense fallback={<div></div>}>
+            <Switch>
+                
+            </Switch>
+        </Suspense>
+    </Router>
+}
