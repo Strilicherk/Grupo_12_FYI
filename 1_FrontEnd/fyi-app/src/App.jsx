4fb0c33d5647fe89f4f01332b1b0ea497f075baa
Samuel Luz Santana <slsamuelluz@gmail.com>
2021-10-19 22:50:02 -0300

    comit striili

--- 1_FrontEnd/fyi-app/src/App.jsx
+++ 1_FrontEnd/fyi-app/src/App.jsx
@@ -2,7 +2,12 @@ import "./App.css";
 import Header from "./components/header";
 import Button from "./components/buttom";
 
-import down from "./components/IMG/down.svg"
+
+import down from "./components/IMG/down.svg";
+import clock from "./components/IMG/clock.svg";
+import alunosicon from "./components/IMG/student.svg";
+import empresaicon from "./components/IMG/empresa.svg"
+
 
 function App() {
   return (
@@ -35,6 +40,39 @@ function App() {
 
           <div className="desce">
             <a href="#"><img src={down} /></a> 
+          </div>
+
+          <div className="container">
+            <div className="solid">
+
+              <div className="treinamento">
+                <img src={clock}/>
+                <div className="down-txt">
+                  <h4>+10000</h4>
+                  <p>Horas de treinamento</p>
+                </div>
+              </div>
+
+              <div className="alunos">
+                <img src={alunosicon} />
+                <div className="down-txt">
+                  <h4>+1000</h4>
+                  <p>Alunos Treinados</p>
+                </div>
+              </div>
+             
+              <div className="empresas">
+                <img src={empresaicon} />
+                <div className="down-txt">
+                  <h4>+50</h4>
+                  <p>Empresas Atendidas</p>
+                </div>
+              </div>
+
+
+
+
+            </div>
           </div>
 
         </main>
