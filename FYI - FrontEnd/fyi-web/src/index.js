05c7e07fcd9f120a098bd981f21b67c57c6f93bf
Gustavo Santos <gustavo.santos211@senaisp.edu.br>
2021-10-21 19:41:16 -0300

    Página Login feita

--- FYI - FrontEnd/fyi-web/src/index.js
+++ FYI - FrontEnd/fyi-web/src/index.js
@@ -12,19 +12,15 @@ import reportWebVitals from './reportWebVitals';
 const routing = (
   <Router>
     <div>
-      <Route path="/login" component={Login}/>
+      <Switch>
+        <Route exact path="/" component={App}/>
+        <Route exact path="/login" component={Login}/>
+      </Switch>
     </div>
   </Router>
 )
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(routing, document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
+
