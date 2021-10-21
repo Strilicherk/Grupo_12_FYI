4c28a4b33edfb4b2271fcbf68c2b2f2835e94459
Gustavo Santos <gustavo.santos211@senaisp.edu.br>
2021-10-20 23:14:04 -0300

    Inputs e imports feitos

--- FYI - FrontEnd/fyi-web/src/index.js
+++ FYI - FrontEnd/fyi-web/src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Route, BrowserRouter as Router, Switch, Redirect} from 'react-router-dom';
+
 import './index.css';
+
 import App from './pages/home/App';
+import Login from './pages/login/login';
+
 import reportWebVitals from './reportWebVitals';
+
+const routing = (
+  <Router>
+    <div>
+      <Route path="/login" component={Login}/>
+    </div>
+  </Router>
+)
 
 ReactDOM.render(
   <React.StrictMode>
--- FYI - FrontEnd/fyi-web/src/pages/login/login.js
+++ FYI - FrontEnd/fyi-web/src/pages/login/login.js
@@ -7,5 +7,37 @@ class Login extends Component{
             email : '',
             senha : ''
         }
+    };
+
+    render(){
+        return(
+            <div>
+                <main>
+                    <section>
+                        <p>Bem-vindo(a)! Faça o login para acessar a página</p>
+                        <form onSubmit={this.efetuaLogin}>
+                            <input type="text" 
+                                   name="email" 
+                                   value={this.state.email} 
+                                   onChange={this.atualizaStateCampo}
+                                   placeholder="Email"
+                                />
+                            <input type="password" 
+                                   name="senha" 
+                                   value={this.state.senha} 
+                                   onChange={this.atualizaStateCampo}
+                                   placeholder="Senha"
+                                />
+
+                                <button type="submit">
+                                    Entrar
+                                </button>
+                        </form>
+                    </section>
+                </main>
+            </div>
+        )
     }
-}
+};
+
+export default Login;
