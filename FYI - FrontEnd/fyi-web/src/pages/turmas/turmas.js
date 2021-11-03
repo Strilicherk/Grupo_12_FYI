a9d2c8352e262e829c3c48e1885c0f2ca6011e6d
Gustavo Henrique da Silva Santos <48737833804@info.local>
2021-11-03 14:36:14 -0300

    Pagina todas as turmas em execução

--- FYI - FrontEnd/fyi-web/src/pages/turmas/turmas.js
+++ FYI - FrontEnd/fyi-web/src/pages/turmas/turmas.js
@@ -1,18 +1,50 @@
 import React, {Component} from 'react';
 
+import '../../assets/css/turmas.css'
+
 class Turmas extends Component{
     render(){
         return(
             <main className="content">
-                <div className="turmasDisponiveis">
+                <section className="todasTurmasDisponiveis">
                     <h1>Todas as Turmas</h1>
-                    <select multiple={true} value={[]}>
+                    <h2>Filtrar por:</h2>
+                    <select className="select">
                     <option value="cargaHoraria">40 Horas</option>
                     <option value="dataInicio">Dezembro</option>
                     <option value="tema">Microsoft Dinamics 360</option>
                     <option value="tema">Microsoft Dinamics 360</option>
                     </select>
-                </div>                
+                </section>
+                <section className="turmasDisponiveis">
+                    <div className="infosGerais">
+                        <div className="textoCurso">
+                            <h2></h2>
+                        </div>
+                        <div className="gradeCurricular">
+                            <h2>Carga Horária</h2>
+                            <p>40 Horas</p>
+                        </div>
+                    </div>
+                    <div className="infosGerais">
+                        <div className="textoCurso">
+                            <h2></h2>
+                        </div>
+                        <div className="gradeCurricular">
+                            <h2>Carga Horária</h2>
+                            <p>40 Horas</p>
+                        </div>
+                    </div>
+                    <div className="infosGerais">
+                        <div className="textoCurso">
+                            <h2></h2>
+                        </div>
+                        <div className="gradeCurricular">
+                            <h2>Carga Horária</h2>
+                            <p>40 Horas</p>
+                        </div>
+                    </div>
+                </section>               
             </main>
         )
     }
