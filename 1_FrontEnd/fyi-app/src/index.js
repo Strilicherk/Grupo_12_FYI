ba5abba705602fba1e91496d5d23ca97789e008f
SamuelLuzSantana <35834520898@info.local>
2021-11-03 15:41:47 -0300

    ao

--- 1_FrontEnd/fyi-app/src/index.js
+++ 1_FrontEnd/fyi-app/src/index.js
@@ -1,29 +1,43 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './Pages/Institucional/home';
 import reportWebVitals from './reportWebVitals';
-import { Route, BrowserRouter as Router, Switch} from  'react-router-dom';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import reactDom from 'react-dom';
 import Home from './Pages/Institucional/home.jsx';
 import Cursos from './Pages/Institucional/cursos.jsx';
 import Processo from './Pages/Institucional/processo.jsx';
 import Contato from './Pages/Institucional/contato.jsx';
+import Login from './Pages/Administrador/login.jsx';
 
-const routing = (
-  <Router>
-     <div>
-       <Switch>
-          <Route exact path="/" component={Home}/>
-          <Route exact path="/cursos" component={Cursos}/>
-          <Route exact path="/processo-seletivo" component={Processo}/>
-          <Route exact path="/contato-duvidas" component={Contato}/>
-       </Switch>
-     </div>
-  </Router>
+const routing = ( <
+    Router >
+    <
+    div >
+    <
+    Switch >
+    <
+    Route exact path = "/"
+    component = { Home }
+    /> <
+    Route exact path = "/cursos"
+    component = { Cursos }
+    /> <
+    Route exact path = "/processo-seletivo"
+    component = { Processo }
+    /> <
+    Route exact path = "/contato-duvidas"
+    component = { Contato }
+    /> <
+    Route exact path = "/Login"
+    component = { Login }
+    />
+
+    <
+    /Switch> <
+    /div> <
+    /Router>
 
 )
 
 reactDom.render(routing, document.getElementById('root'))
-
-
