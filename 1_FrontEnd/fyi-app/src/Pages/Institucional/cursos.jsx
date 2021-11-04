765c9464bbdb65d6198b8674b179c9fb377c84b2
SamuelLuzSantana <35834520898@info.local>
2021-11-04 17:08:23 -0300

    cursos edit

--- 1_FrontEnd/fyi-app/src/Pages/Institucional/cursos.jsx
+++ 1_FrontEnd/fyi-app/src/Pages/Institucional/cursos.jsx
@@ -1,36 +1,74 @@
 import React, { Component } from "react";
 
 //css
-import "../../assets/styles/cursos.css"
+import "../../assets/styles/cursos.css";
 
 //componets
 import Header from "../../components/header";
+import Button from "../../components/buttom";
 
+//Imagens
+import microSo from "../../assets/img/microsoftt.svg";
+import { queryByAttribute } from "@testing-library/dom";
 
 class Cursos extends Component {
     render() {
         return (
             <>
                 <div className="container">
-                    <Header/>
+                    <Header />
                 </div>
 
                 <div className="areas-container">
 
-                <div className="area-aling">
+                    <div className="area-aling">
 
 
-                    <div className="texto-area container">
-                        <p>Áreas de Interesse</p>
-                        <p>Aproveite para aprender mais sobre :</p>
+                        <div className="conteudo-curso container">
+                            <div className="texto-sobre-curso">
+                                <p>Áreas de interes</p>
+                                <p>Aproveite para saber mais sobre :</p>
+                            </div>
+
+                            <div className="card-area">
+                                <div className="card-da-area">
+                                    
+                                </div>
+
+                                <div className="card-da-area">
+
+                                </div>
+
+                                <div className="card-da-area">
+
+                                </div>
+
+                                <div className="card-da-area">
+
+                                </div>
+                            </div>
+
+                            <Button>Todas as turmas</Button>
+
+
+                        </div>
+
+
+                    </div>
+
+                    <div className="microsoft-bottom container">
+                        <img src={microSo} alt="logo da microsoft" />
                     </div>
 
 
-                    
-                </div>
 
 
                 </div>
+
+
+
+
+
 
 
 
