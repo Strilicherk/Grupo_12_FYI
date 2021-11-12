09c05f699bd06451fbc85d8d908a8813d38f4310
Your Name <you@example.com>
2021-11-12 16:16:58 -0300

    Formulário de Inscrição finalizado

--- 1_FrontEnd/fyi-app/src/Pages/Institucional/inscricaocurso.jsx
+++ 1_FrontEnd/fyi-app/src/Pages/Institucional/inscricaocurso.jsx
@@ -5,6 +5,10 @@ import Footer from '../../components/footer';
 import Button from '../../components/buttom';
 
 import crs from '../../assets/img/Card Curso.png';
+import cur from '../../assets/img/curso.png';
+import dat from '../../assets/img/data.png';
+import dur from '../../assets/img/duracao.png';
+import cat from '../../assets/img/categoria.png';
 
 import '../../assets/styles/inscricaocurso.css';
 
@@ -14,7 +18,7 @@ class InscricaoCurso extends Component{
             <>
             <main className="container">
                 <Header/>
-                <div className="Alinhamento">
+                <div className="alinhamento">
                     <section className="sobreCurso">
                         <div className="informCurso">
                             <h1 className="tituloCurso">Power BI Completo:</h1>
@@ -34,9 +38,49 @@ class InscricaoCurso extends Component{
                                 <input className="input" type="text" name="data" placeholder="Data de nascimento"/><br></br>
                                 <input className="input" type="text" name="telefone" placeholder="Telefone"/>
                                 <div className="botao-cl">
-                                    <Button>Inscreva-se</Button>
+                                <Button>Inscreva-se</Button>
                                 </div>
                             </div>
+                        </div>
+                    </section>
+                </div>
+                <div className="alinhamento2">
+                    <div className="publicoAlvo">
+                        <h3>Público alvo</h3>
+                        <p>Voltado para consultores funcionais e técnicos; usuários e gestores da área de negócios e profissionais de TI.</p>
+                    </div>
+                    <div className="preRequisto">
+                    <h3>Pré-requisitos</h3>
+                        <p>Não há pré-requisitos.</p>
+                    </div>
+                </div>
+                <div className="alinhamento3">
+                    <section className="infogeral">
+                        <img src={cur}/>
+                        <div className="textInfo">
+                            <p>Curso</p>
+                            <h3>Online</h3>
+                        </div>
+                    </section>
+                    <section className="infogeral">
+                        <img src={dat}/>
+                        <div className="textInfo">
+                            <p>Data de início</p>
+                            <h3>21/12/2021</h3>
+                        </div>
+                    </section>
+                    <section className="infogeral">
+                        <img src={dur}/>
+                        <div className="textInfo">
+                            <p>Duração</p>
+                            <h3>40 Horas</h3>
+                        </div>
+                    </section>
+                    <section className="infogeral">
+                        <img src={cat}/>
+                        <div className="textInfo">
+                            <p>Categoria</p>
+                            <h3>Power Plataform</h3>
                         </div>
                     </section>
                 </div>
